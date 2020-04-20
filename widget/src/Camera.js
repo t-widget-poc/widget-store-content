87dfa11748a39c85df705ea3170e1927ac1263b8
ykgk518 <y.518gaku@gmai.com>
2020-04-20 17:12:31 +0900

    カメラの幅と高さ変更

--- widget/src/Camera.js
+++ widget/src/Camera.js
@@ -4,6 +4,10 @@ import Webcam from "react-webcam";
 class Camera extends React.Component {
     render() {
         const videoConstraints = {
+            video: {
+                width: { ideal: 1280 },
+                height: { ideal: 720 }
+            },
             facingMode: "user"
         };
         return (

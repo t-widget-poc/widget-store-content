18c2503842944e6657b7d90f8ef72108e0f1ee69
ykgk518 <y.518gaku@gmai.com>
2020-04-21 20:00:15 +0900

    videoタグをReactで埋め込む実装

---
+++ widget/src/Camera/Video.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Video extends React.Component {
+    render() {
+        return (
+            <video id="video" autoplay playsinline></video>
+        )
+        
+    }
+}
+
+export default Video
--- widget/src/firebase/firebase.js
+++ widget/src/firebase/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 const config = {
-    apiKey: "***",
-    authDomain: "***",
+    apiKey: "AIzaSyCha6Xc8JnngcLpKZK_NGYPohq4ZjU1ZZ4",
+    authDomain: "widget-demo-miniprogram.firebaseapp.com",
     databaseURL: "https://widget-demo-miniprogram.firebaseio.com",
-    projectId: "***",
-    storageBucket: "***",
-    messagingSenderId: "***",
-    appId: "***"
+    projectId: "widget-demo-miniprogram",
+    storageBucket: "widget-demo-miniprogram.appspot.com",
+    messagingSenderId: "797424423203",
+    appId: "1:797424423203:web:d7074c80dbaf4a84c7fc96"
 }
 
 firebase.initializeApp(config)
